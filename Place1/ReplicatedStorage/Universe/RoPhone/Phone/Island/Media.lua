5f2621cd693a128e0efc0e7e3b78eef422d2e2ab
Fittergem <99085141+fittergem@users.noreply.github.com>
2025-03-11 19:00:36 -0400

    Updated Place1/ReplicatedStorage/Universe/RoPhone/Phone/Island/Media.lua

---
+++ Place1/ReplicatedStorage/Universe/RoPhone/Phone/Island/Media.lua
@@ -0,0 +1,23 @@
+-- @ScriptType: ModuleScript
+-- @ScriptType: ModuleScript
+local Media = {}
+Media.__index = Media
+
+function Media.new(title: string, subtitle: string, audioId: number, duration: number, sound: Sound)
+  local self = setmetatable({}, Media)
+
+  self.Title = title
+  self.Subtitle = subtitle
+  self.AudioId = "rbxassetid://"..audioId
+
+  self.Sound = sound
+  self.Sound.SoundId = self.AudioId
+  
+  return self
+end
+
+function Media:Play()
+  self.Sound:Play()
+end
+
+return Media
