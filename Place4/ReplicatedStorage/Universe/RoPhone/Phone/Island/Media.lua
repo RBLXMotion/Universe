82405270641f5bc52c236bc1979b67fdc060658c
Fittergem <99085141+fittergem@users.noreply.github.com>
2025-03-12 20:00:26 -0400

    Updated Place4/ReplicatedStorage/Universe/RoPhone/Phone/Island/Media.lua

--- Place4/ReplicatedStorage/Universe/RoPhone/Phone/Island/Media.lua
+++ Place4/ReplicatedStorage/Universe/RoPhone/Phone/Island/Media.lua
@@ -1,24 +1,28 @@
 -- @ScriptType: ModuleScript
 -- @ScriptType: ModuleScript
--- @ScriptType: ModuleScript
+
 local Media = {}
 Media.__index = Media
 
 function Media.new(title: string, subtitle: string, audioId: number, duration: number, sound: Sound)
-  local self = setmetatable({}, Media)
+	local self = setmetatable({}, Media)
 
-  self.Title = title
-  self.Subtitle = subtitle
-  self.AudioId = "rbxassetid://"..audioId
+	self.Title = title
+	self.Subtitle = subtitle
+	self.AudioId = "rbxassetid://"..audioId
 
-  self.Sound = sound
-  self.Sound.SoundId = self.AudioId
-  
-  return self
+	self.Sound = sound
+	self.Sound.SoundId = self.AudioId
+
+	return self
 end
 
 function Media:Play()
-  self.Sound:Play()
+	self.Sound:Play()
+end
+
+function Media:Stop()
+	self.Sound:Stop()
 end
 
 return Media
