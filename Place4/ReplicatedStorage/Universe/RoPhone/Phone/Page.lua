4efc860afda3547f52ec3e6090e3aeb49a8d109f
Fittergem <99085141+fittergem@users.noreply.github.com>
2025-03-18 14:40:27 -0400

    Update Page.lua

--- Place4/ReplicatedStorage/Universe/RoPhone/Phone/Page.lua
+++ Place4/ReplicatedStorage/Universe/RoPhone/Phone/Page.lua
@@ -15,8 +15,8 @@ function Page.new(homescreen: CanvasGroup)
 	self.Frame.Transparency = 1
 
 	self.GridFrame = Instance.new("Frame", self.Frame)
-	self.GridFrame.Position = UDim2.new(.5,0, (CONFIG.ISLAND_SIZE.Y + CONFIG.DOCK_SIZE.Y/2), 0)
-	self.GridFrame.Size = UDim2.new(.9,0, 1 - (CONFIG.ISLAND_SIZE.Y + CONFIG.GESTURE_SIZE.Y + CONFIG.DOCK_SIZE.Y + CONFIG.PAGE_DOT_SIZE.Y), 0)
+	self.GridFrame.Position = UDim2.new(.5,0, (CONFIG.ISLAND_SIZE.Y + CONFIG.ISLAND_MARGIN), 0)
+	self.GridFrame.Size = UDim2.new(.9,0, 1 - (CONFIG.ISLAND_SIZE.Y + CONFIG.ISLAND_MARGIN + CONFIG.GESTURE_SIZE.Y + CONFIG.GESTURE_MARGIN + CONFIG.DOCK_SIZE.Y + CONFIG.DOCK_MARGIN + CONFIG.PAGE_DOT_SIZE.Y), 0)
 	self.GridFrame.AnchorPoint = Vector2.new(.5,0)
 	self.GridFrame.Transparency = 1
 
