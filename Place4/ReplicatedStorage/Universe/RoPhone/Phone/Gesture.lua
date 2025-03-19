1c0a9f60da5a1252e0d2dca7ec22451f40396e83
Fittergem <99085141+fittergem@users.noreply.github.com>
2025-03-18 23:56:00 -0400

    Update Gesture.lua

--- Place4/ReplicatedStorage/Universe/RoPhone/Phone/Gesture.lua
+++ Place4/ReplicatedStorage/Universe/RoPhone/Phone/Gesture.lua
@@ -1,6 +1,7 @@
 -- @ScriptType: ModuleScript
--- @ScriptType: ModuleScript
--- @ScriptType: ModuleScript
+
+local CONFIG = require(script.Parent.Parent:WaitForChild("CONFIG"))
+
 local Gesture = {}
 Gesture.__index = Gesture
 
@@ -9,13 +10,14 @@ function Gesture.new()
 	
 	self.Button = Instance.new("TextButton")
 	self.Button.Name = "GestureBar"
-	self.Button.AnchorPoint = Vector2.new(.5,.5)
-	self.Button.Position = UDim2.new(.5,0,.98,0)
-	self.Button.Size = UDim2.new(.6,0,.015,0)
-	self.Button.ZIndex = 100
+	self.Button.AnchorPoint = Vector2.new(.5,1)
+	self.Button.Position = UDim2.new(.5,0,1 - CONFIG.GESTURE_MARGIN,0)
+	self.Button.Size = UDim2.new(CONFIG.GESTURE_SIZE.X,0,CONFIG.GESTURE_SIZE.Y,0)
+	self.Button.ZIndex = 200
 	self.Button.BackgroundColor3 = Color3.new(1,1,1)
 	self.Button.Text = ""
 	self.Button.AutoButtonColor = false
+	self.Button.Visible = false
 	
 	self.Corner = Instance.new("UICorner", self.Button)
 	self.Corner.CornerRadius = UDim.new(1,0)
